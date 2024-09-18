bbc2f838e21e0557bbc2aff8c1a8fc245a8d4705
chajus1 <chajus1@googlemail.com>
2024-09-18 22:10:17 +0100

    Add project files.

---
+++ RandomDrive/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace RandomDrive
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int randomInt = Random.Shared.Next();
+            Console.WriteLine(randomInt);
+            int zeroToNine = Random.Shared.Next(10);
+            Console.WriteLine(zeroToNine);
+            int dieRoll = Random.Shared.Next(1, 7);
+            Console.WriteLine(dieRoll);
+
+            double randomDouble = Random.Shared.NextDouble();
+            Console.WriteLine(randomDouble * 10);
+
+        }
+    }
+}
